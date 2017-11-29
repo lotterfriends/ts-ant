1eb24eb8f62a8a4fb0c01b4ce505a7150c5c756a
André Tarnowsky <andre@tarnowsky.de>
2017-11-29 21:43:15 +0100

    slow the bug down

--- src/bug.ts
+++ src/bug.ts
@@ -2,7 +2,7 @@ import { LivingObject } from './livingObject';
 import { Board } from './board';
 
 export class Bug extends LivingObject {
-    protected speed: number = .35;
+    protected speed: number = .25;
 
     constructor() {
         super(Board.randomPosition(20), 10, 20, Board.getRandomAngle());
@@ -11,7 +11,7 @@ export class Bug extends LivingObject {
 
     live(turn: number) {
 
-        if (Math.random() * 100 > 99) {
+        if (Math.random() * 1000 > 990) {
             this.rotate(Board.getRandomAngle());
         }
 
